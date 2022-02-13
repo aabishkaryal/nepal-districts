31d588d97a41c50d64e1b8b24538d1c6b06e5969
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-02-13 11:17:14 +0545

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
