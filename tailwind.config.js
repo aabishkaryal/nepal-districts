d0815a3001aa627d151d022589ae557074d11eba
ashiishme <iamashiishy@gmail.com>
2022-02-13 17:23:46 +0100

    build: add custom keyframes animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'draw-path': 'draw-path 3.5s 1 linear forwards',
+      },
+      keyframes: {
+        'draw-path': {
+          '100%': {
+            'stroke-dashoffset': '0%',
+          },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
