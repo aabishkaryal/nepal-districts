29d101a8fe7088e9fc29d2363ac5761fae17e397
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-02-13 11:53:50 +0545

    styling and input box done

--- components/DistrictMap.tsx
+++ components/DistrictMap.tsx
@@ -3,7 +3,7 @@ import District from './District'
 
 export default function DistrictMap() {
   return (
-    <div className="my-10 w-full max-w-[1000px] px-5">
+    <div className="w-full max-w-[1024px] px-6">
       <svg viewBox="0 0 1026.077 519.136" className="w-full">
         <g transform="translate(-52.379 -15.971)">
           {DistrictData.map((district) => (
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,17 @@ import DistrictMap from '../components/DistrictMap'
 
 export default function Home() {
   return (
-    <div className="my-10 flex flex-col items-center justify-center">
-      Can you name all the districts in Nepal?
+    <div className="flex h-screen w-screen flex-col items-center justify-center space-y-10">
+      <h1 className="px-4 text-center text-xl font-bold sm:text-2xl md:text-3xl">
+        How many districts of Nepal can you name?
+      </h1>
+
+      <input
+        type="text"
+        placeholder="Try 'Kathmandu' or 'kathmandu'"
+        className="w-full max-w-[320px] rounded-md border-2 border-orange-500 px-4 py-2 transition-all"
+      />
+
       <DistrictMap />
     </div>
   )
