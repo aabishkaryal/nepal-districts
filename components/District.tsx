1c9d13dcbff6a35431c3e898257d4587830a27c3
Binit Ghimire <thebinitghimire@gmail.com>
2022-02-14 20:32:52 +0545

    Implementation of District Colors, based on Province! #10

--- components/District.tsx
+++ components/District.tsx
@@ -1,13 +1,18 @@
 type props = {
+  province: number;
   shape: string;
 };
 
-export default function District({ shape }: props) {
+const colors: string[] = Array(7)
+  .fill('#')
+  .map((c) => c + Math.random().toString(16).substring(2,8));
+
+export default function District({ province, shape }: props) {
   return (
     <path
       className="svg-property animate-draw-path"
       stroke={'#000'}
-      fill="#fff"
+      fill={colors[province-1]}
       d={shape}
     />
   );
