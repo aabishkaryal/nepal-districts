0dffbcc9a88d4af2001d1619bfca0cdb65645e20
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-02-28 21:31:40 +0545

    index hero and call to action complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,32 @@
-import Header from 'components/Header';
+import NepalMap from 'components/NepalMap';
+import Link from 'next/link';
 
 export default function Index() {
-  return <></>;
+  return (
+    <div className="flex flex-col justify-center">
+      {/* Hero */}
+      <div className="my-8 space-y-2 px-4">
+        <h1 className="bold text-justify text-3xl">
+          How well do you know Nepal?
+        </h1>
+        <p className="text-xs text-gray-500">
+          This is a little fun website to test how well you know the geography
+          of Nepal.
+        </p>
+      </div>
+      {/* Call to action */}
+      <div className="flex w-full flex-nowrap justify-evenly">
+        <Link href="/casual">
+          <div className="w-[115px] rounded-md bg-blue-500 px-3 py-1 text-center text-xl text-white shadow-sm">
+            Casual
+          </div>
+        </Link>
+        <Link href="/hardcore">
+          <div className="w-[115px] rounded-md bg-blue-500 px-3 py-1 text-center text-xl text-white shadow-sm">
+            Hardcore
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
