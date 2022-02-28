d23d4dddb407662bee862ce77ad582be32763227
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-02-28 19:31:58 +0545

    Custom favicon with custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/Flag_of_Nepal.ico" sizes="any" />
+        <link rel="icon" href="/Flag_of_Nepal.svg" type="image/svg+xml" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
