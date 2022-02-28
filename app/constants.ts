c8e4aca48a21bd7262ee78debd0194ec5b1c714b
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-02-28 19:04:26 +0545

    constant colors for provinces

---
+++ app/constants.ts
@@ -0,0 +1,9 @@
+export const COLORS = [
+  '#F08366',
+  '#FCF683',
+  '#AFCB1E',
+  '#A2DBF9',
+  '#D6B56C',
+  '#F7B7D7',
+  '#A8D4B1',
+];
--- components/District.tsx
+++ components/District.tsx
@@ -1,18 +1,16 @@
+import { COLORS } from 'app/constants';
+
 type props = {
   province: number;
   shape: string;
 };
-
-const colors: string[] = Array(7)
-  .fill('hsl(_)')
-  .map((c) => c.replace(/_/g, `${Math.floor(Math.random()*360)}, 100%, 50%`));
 
 export default function District({ province, shape }: props) {
   return (
     <path
       className="svg-property animate-draw-path"
       stroke={'#000'}
-      fill={colors[province-1]}
+      fill={COLORS[province - 1]}
       d={shape}
     />
   );
