8d7cd725172e5b99cc3e61bf00c98d07d08c7c25
Aabishkar Aryal <45176384+aabishkaryal@users.noreply.github.com>
2022-03-03 17:12:01 +0545

    responsive padding for map

--- components/NepalMap.tsx
+++ components/NepalMap.tsx
@@ -3,16 +3,14 @@ import District from 'components/District';
 
 export default function NepalMap() {
   return (
-    <div className="w-full">
-      <div className="w-full max-w-[1024px] px-6">
-        <svg viewBox="0 0 1026.077 519.136" className="w-full">
-          <g transform="translate(-52.379 -15.971)">
-            {DistrictData.map((district) => (
-              <District {...district} key={district.zip} />
-            ))}
-          </g>
-        </svg>
-      </div>
+    <div className="w-full max-w-[1024px] px-2 sm:px-4 md:px-6">
+      <svg viewBox="0 0 1026.077 519.136" className="w-full">
+        <g transform="translate(-52.379 -15.971)">
+          {DistrictData.map((district) => (
+            <District {...district} key={district.zip} />
+          ))}
+        </g>
+      </svg>
     </div>
   );
 }
